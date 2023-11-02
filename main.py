7739ff16aeb4af24292055672cbcd69611a42fac
Raymond <z5360323@ad.unsw.edu.au>
2023-11-03 01:16:09 +1100

    added some comments on my observations of the reward

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ while True:
     if (keyboard.is_pressed(" ")):
         _, reward, _ = game_state.frame_step(True)
         print(reward)
+        # currently 0.1 reward by default, 1 reward if bird passes through pipe, -1 reward if bird hits pipe or ground
+        # keyboard seems to press jump multiple times but whatever.
     else:
         _, reward, _ = game_state.frame_step(False)
     if (keyboard.is_pressed("q")):
