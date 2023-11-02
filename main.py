6b5c43896d4c16c7d7b9b13009006440aee63d14
Raymond <z5360323@ad.unsw.edu.au>
2023-11-02 23:48:13 +1100

    forgor keyboard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import game.wrapped_flappy_bird as game
+import keyboard
 
 game_state = game.GameState()
 while True:
     game_state.frame_step(True)
+    if (keyboard.is_pressed("q")):
+        break
 
